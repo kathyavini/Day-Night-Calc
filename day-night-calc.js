937ce5ee90bd3a6e6ffcdcf0093215764a16d52d
kathyavini <kathyavini@gmail.com>
2021-11-18 19:35:31 -0800

    Change time of day console logging to just a hint

--- day-night-calc.js
+++ day-night-calc.js
@@ -13,54 +13,54 @@ let removeTheme = '';
 // const testSunset = new Date('Nov 18, 2021 17:24:00');
 // const testNight = new Date('Nov 18, 2021 22:24:00');
 
-console.log("In Vancouver...");
-
 // Before Sunrise
 if (now.getTime() <= times.nauticalDawn.getTime()) {
-    console.log("It's night");
+    // console.log("It's night");
     background = 'url("./img/Valley-night.svg")';
     colorTheme = '';
 } else {
-    console.log("It's not yet night");
+    // console.log("It's not yet dawn");
 }
 
 // Nautical dawn (image is same a sunset)
 if (now.getTime() >= times.nauticalDawn.getTime()) {
-    console.log("It's later than nautical Dawn");
+    // console.log("It's later than nautical Dawn");
     background = 'url("./img/Valley-sunset.svg")';
     colorTheme = '';
 } else {
-    console.log("It's not yet nautical Dawn");
+    // console.log("It's not yet nautical Dawn");
 }
 
 // Daytime (after sunrise)
 if (now.getTime() >= times.sunriseEnd.getTime()) {
-    console.log("It's later than sunrise");
+    // console.log("It's later than sunrise");
     background = 'url("./img/Valley-day.svg")';
     colorTheme = 'day';
 } else {
-    console.log("It's not yet sunrise");
+    // console.log("It's not yet sunrise");
 }
 
 // Sunset
 if (now.getTime() >= times.sunsetStart.getTime()) {
-    console.log("It's later than sunset");
+    // console.log("It's later than sunset");
     background = 'url("./img/Valley-sunset.svg")';
     colorTheme = '';
     removeTheme = 'day';
 } else {
-    console.log("It's not yet sunset");
+    // console.log("It's not yet sunset");
 }
 
 // Night
 if (now.getTime() >= times.night.getTime()) {
-    console.log("It's night");
+    // console.log("It's night");
     background = 'url("./img/Valley-night.svg")';
     colorTheme = '';
 } else {
-    console.log("It's not yet night");
+    // console.log("It's not yet night");
 }
 
 container.style.backgroundImage = background;
 if (colorTheme) root.classList.add(colorTheme);
 if (removeTheme) root.classList.remove(removeTheme);
+
+console.log("Hint: Look at the value of times.nauticalDawn/sunriseEnd/sunsetStart/night to find out when the theme will change");
