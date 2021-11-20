cc4617c562c0bea8e1d2cc84e6e9a8d331268b02
kathyavini <kathyavini@gmail.com>
2021-11-20 13:27:13 -0800

    Add power, pi, and percent functionality and refine display truncating based on font and display size

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+let calculator = document.querySelector('.calculator');
 let display = document.querySelector('.display-main');
 let preview = document.querySelector('.display-preview');
 let numbers = document.querySelectorAll('.number');
@@ -9,38 +10,79 @@ let subtract = document.querySelector('.subtract');
 let divide = document.querySelector('.divide');
 let multiply = document.querySelector('.multiply');
 let equals = document.querySelector('.equals');
+let power = document.querySelector('.power');
+let percent = document.querySelector('.percent');
+let pi = document.querySelector('.pi');
 
 
 // Some adjusts for android font sizes and small screens
-// (MOSTLY respect large font sizing but truncate values more)
+// Respect large system font but truncate values so they fit
+
 let floatSci;
 let floatDec;
 let maxLength;
 
-// Test for small android devices on largest font (but not landscape)
-computedFont = window.getComputedStyle(display, null).getPropertyValue('font-size').slice(0,-2);
-computedWidth = window.getComputedStyle(display, null).getPropertyValue('width').slice(0,-2);
+function testDisplaySetFloats() {
+    let computedFont = window.getComputedStyle(display, 
+            null).getPropertyValue('font-size').slice(0,-2);
+    let computedWidth = window.getComputedStyle(calculator, 
+            null).getPropertyValue('width').slice(0,-2);
+    let computedHeight = window.getComputedStyle(calculator, 
+            null).getPropertyValue('height').slice(0,-2);
 
-if (computedFont > 70 && computedWidth <= 300) {
-    floatSci = 3;
-    floatDec = 6;
-    maxLength = 9;
-} else if (computedFont > 60 && computedWidth <= 300) {
-    floatSci = 3;
-    floatDec = 7;
-    maxLength = 11;
-} else {
-    floatSci = 4;
-    floatDec = 9;
-    maxLength = 13;
+    // dimensions are slow to update after re-orientation
+    if (screen.orientation && 
+            (screen.orientation.type == "portrait-primary") && 
+            computedWidth > computedHeight) {
+        computedWidth = computedHeight;
+    }
+
+    // Test for small screens on largest font
+    if (computedFont > 70 && computedWidth <= 340) {
+        floatSci = 5;
+        floatDec = 6;
+        maxLength = 11;
+    } else if (computedFont > 60 && computedWidth <= 340) {
+        floatSci = 6;
+        floatDec = 7;
+        maxLength = 12;
+    } else {
+        floatSci = 7;
+        floatDec = 9;
+        maxLength = 13;
+    }
+
+    // values can be longer in landscape mode for browsers that support this
+    if (screen.orientation && 
+            (screen.orientation.type == "landscape-primary") && 
+            screen.orientation.angle == 90) {
+        if (computedFont > 70 && computedHeight <= 340) {
+            floatSci = 7;
+            floatDec = 13;
+            maxLength = 15;
+        } else {
+            floatSci = 10;
+            floatDec = 15;
+            maxLength = 18;
+        }
+    }
+
 }
+
+testDisplaySetFloats();
+
+// For supporting browsers:
+if (screen.orientation) {
+    screen.orientation.addEventListener('change', testDisplaySetFloats);
+}
+
 
 const calc = {
     'inputStr':'',
     'ans': 0,
     'displayStr':'',
-    'displayStrDec':'',
     'displayStrNonDec':'',
+    'displayStrDec':'',
     'previewValue': 0,
     'previewStr':'',
     'lastOperator':'',
@@ -65,6 +107,7 @@ function updatePreview() {
 }
 
 // Add event listeners
+// Numbers
 for (let each of numbers) {
     each.addEventListener('click', () => {
         calc.inputStr = calc.inputStr + each.textContent;
@@ -77,7 +120,7 @@ for (let each of numbers) {
     });
 }
 
-// Function buttons
+// Functions
 clear.addEventListener('click', () => {
     for (key in calc) {
         calc[key] = '';
@@ -109,8 +152,20 @@ add.addEventListener('click', () => {
 })
 
 subtract.addEventListener('click', () => {
+    // Use to make negatives as well
+    if (calc.inputStr.length === 0 && 
+            calc.lastOperator !== returnAns) { // after equals you can subtract
+        calc.inputStr = '-';
+        updateDisplay();
+        return;
+    } else if (calc.inputStr === '-') { // allowing a toggle
+        calc.inputStr = '';
+        updateDisplay();
+        return;
+    }
     operate(subtraction);
 })
+
 
 divide.addEventListener('click', () => {
     operate(division);
@@ -120,6 +175,62 @@ multiply.addEventListener('click', () => {
     operate(multiplication);
 })
 
+power.addEventListener('click', () => {
+    operate(raisePower);
+});
+
+percent.addEventListener('click', () => {
+    if (calc.inputStr && calc.inputStr !== '-') {
+        calc.inputStr = ((+calc.inputStr)/100).toString();
+        updateDisplay();
+    } else if (calc.lastOperator == returnAns && calc.operand1) {
+        calc.operand1 = calc.operand1/100;
+        calc.displayStr = calc.operand1.toString();
+        truncateDecimals();
+        updateDisplay();
+    }
+});
+
+pi.addEventListener('click', () => {
+    if ((calc.inputStr.length === 0 || calc.inputStr === '-') && 
+            calc.lastOperator !== returnAns) {
+        calc.previewStr = calc.inputStr + '\u03C0';
+        calc.inputStr += (Math.PI.toFixed(floatDec)).toString();
+    } else if ((calc.inputStr.length === 0 || calc.inputStr === '-') && 
+            calc.lastOperator === returnAns) {
+        
+        // Format for preview first
+        calc.displayStr = calc.ans.toString();
+        truncateDecimals();
+        calc.previewStr = calc.displayStr + '\u03C0';
+        calc.displayStr = '';
+
+        // Format input as well
+        calc.ans = calc.ans * Math.PI;
+        calc.inputStr = calc.ans.toString();
+        calc.displayStr = calc.inputStr;
+        truncateDecimals();
+        calc.inputStr = calc.displayStr;
+        calc.displayStr = '';
+        calc.operand1 = 0;
+    
+    } else {
+        // Format for preview first
+        calc.displayStr = calc.inputStr;
+        truncateDecimals();
+        calc.previewStr = calc.displayStr + '\u03C0';
+
+        // Next format input display
+        calc.inputStr = (+calc.inputStr * Math.PI).toFixed(floatDec).toString();
+        calc.displayStr = calc.inputStr; // to format
+        truncateDecimals();
+        calc.inputStr = calc.displayStr;
+        calc.displayStr = '';
+    }
+    updatePreview();
+    updateDisplay();
+});
+
 equals.addEventListener('click', () => {
     operate(returnAns);
     // If next key is a number, the operands should clear
@@ -128,17 +239,23 @@ equals.addEventListener('click', () => {
 
 // Two operand functionality
 function operate(mathFunction) {
+    calc.previewStr = ''; // for pi display
+    updatePreview();
+
+    // console.log(`Operand 1 is ${calc.operand1}`);
+    // console.log(`Operand 2 is ${calc.operand2}`);
 
     /* --- Make more robust to bad input ----*/
-    // Pressing equals or other function buttons without input should do nothing
-    if (!calc.inputStr && mathFunction !== returnAns && 
-                calc.lastOperator !== returnAns) { // except for right after equals
+    // Pressing function buttons without number input should do nothing
+    // Except if you have just returned an ans with equals
+    if ((!calc.inputStr || calc.inputStr == '-') && mathFunction !== returnAns && 
+                calc.lastOperator !== returnAns) {
         // But you can update the operation being performed
         calc.lastOperator = mathFunction;
         return;
-    } else if (!calc.inputStr && mathFunction == returnAns && 
+    // ignore an equals after an operation completely
+    } else if ((!calc.inputStr || calc.inputStr == '-') && mathFunction == returnAns && 
         calc.lastOperator !== returnAns) {
-        // ignore an equals after an operation completely
         return;
     }
     /* -----------------------------------*/
@@ -163,12 +280,6 @@ function operate(mathFunction) {
     }
 }
 
-function appendToValue(item, value) {
-    return value + item;
-}
-
-
-
 
 // Prettify functions
 function splitAtDecimal() {
@@ -181,36 +292,49 @@ function splitAtDecimal() {
         calc.displayStrNonDec = calc.displayStr.substring(decIndex);
     }
 }
+
 function truncateDecimals() {
 
     splitAtDecimal();
 
-     // Maintain existing scientific notation; don't use toFixed() then    
+    // console.log("about to truncate!");
+    // console.log(calc);
+    // console.log(`Display String Length is ${calc.displayStr.length} chars long`)
+    // console.log(`Display String Non-Decimal is ${calc.displayStrNonDec.length} chars long`)
+    // console.log(`Display String Decimal is ${calc.displayStrDec.length} chars long`);
+
+     // Maintain existing scientific notation for decimals
      if (calc.displayStrDec.length > floatSci && 
                 calc.displayStr.search((/[e]/g)) !== -1) {
         calc.displayStr = ((+calc.displayStr).toExponential(floatSci)).toString();
         return;
     }
 
-    // Truncate so that decimals + nonDecimals < maxLength
-    if (calc.displayStrDec.length > floatDec && 
-            calc.displayStrNonDec.length <= 2) { // all decimals
+    // For a number that's all decimals
+    // Six is kind of arbitrary but it seems like the max
+    // precision the  ans variable tends to store
+    if (calc.displayStrDec.length >= 6 && 
+            calc.displayStrNonDec.length <= 2) {
         shortFloat = (+calc.displayStr).toFixed(floatDec);
         calc.displayStr = shortFloat.toString();
-    } else if (calc.displayStrDec.length > floatDec && 
+    // Number that is all decimals plus medium length number
+    } else if (calc.displayStr.length > maxLength && 
                 calc.displayStrNonDec.length > 2 && 
-                calc.displayStrNonDec.length <= maxLength - 6) { // mostly decimals
+                calc.displayStrNonDec.length <= (maxLength - 6)) { // mostly decimals
         shortFloat = (+calc.displayStr).toFixed(floatDec - 3);
         calc.displayStr = shortFloat.toString();
-    } else if (calc.displayStrDec.length > 4 && 
+    // Long decimals in a fairly long number
+    } else if (calc.displayStr.length > maxLength && 
                 calc.displayStrNonDec.length > (maxLength - 6)) {
         shortFloat = (+calc.displayStr).
-            toFixed(Math.max(2, maxLength - calc.displayStrNonDec.length));
+            toFixed(Math.max(2, maxLength - calc.displayStrNonDec.length - 1));
         calc.displayStr = shortFloat.toString();
-    } else
+    }
     
     // Apply scientific notation to large numbers
-    if (calc.displayStr.length > maxLength) {
+    if (calc.displayStrNonDec.length > maxLength || 
+            (calc.displayStrDec.length > 0 && 
+                calc.displayStrNonDec.length > (maxLength - 3))) {
         calc.displayStr = ((+calc.displayStr).toExponential(floatSci)).toString();
         // But remove that odd plus formatting
         plusIndex = calc.displayStr.search((/[+]/g));
@@ -221,8 +345,12 @@ function truncateDecimals() {
 }
 
 function addCommas() {
-
     splitAtDecimal();
+
+    // If it's become large for JavaScript just return it
+    if (calc.displayStr === 'Infinity') {
+        return;
+    }
 
     // Might have been better to work with the number Value, but...
     if (calc.displayStrNonDec.length > 0) {
@@ -232,21 +360,31 @@ function addCommas() {
     
     if (calc.displayStrNonDec.length > 3) {
         let commasString = '';
-        while (calc.displayStrNonDec.slice(0, -3).length > 0) {
-            commasString += ','+ calc.displayStrNonDec.slice(-3);
-            calc.displayStrNonDec = calc.displayStrNonDec.slice(0,-3);
+        let negSign = '';
+
+
+        // save the negative sign but don't include in commas!
+        if (calc.displayStrNonDec[0] === '-') {
+            negSign = '-';
+            calc.displayStrNonDec = calc.displayStrNonDec.slice(1);
+        }
+
+        let tempNonDecStr = calc.displayStrNonDec;
+        while (tempNonDecStr.slice(0, -3).length > 0) {
+            commasString = ','+ tempNonDecStr.slice(-3) + commasString;
+            tempNonDecStr = tempNonDecStr.slice(0, -3);
         }
         // add any left over digits to the start
-        commasString = calc.displayStrNonDec + commasString;
+        commasString = tempNonDecStr + commasString;
         // And re-create final string
-        calc.displayStr = commasString + calc.displayStrDec;
+        calc.displayStr = negSign + commasString + calc.displayStrDec;
     }
 }
 
 function adjustDisplayFontSize() {
-    if (calc.displayStr.length > 11) {
+    if (calc.displayStr.length > (maxLength - 2)) {
         display.style.fontSize = '36px';
-    } else if (calc.displayStr.length > 9) {
+    } else if (calc.displayStr.length > (maxLength - 4)) {
         display.style.fontSize = '42px';
     } else display.style.fontSize = '54px'; // the default-largest
 }
@@ -293,10 +431,6 @@ function factorial(a, b) {
     }
     return ans;
 };
-
-function makePercent(a) {
-    return a/100;
-}
 
 function returnAns(a, b) {
     return a;
@@ -366,9 +500,16 @@ window.addEventListener("keydown", (ev) => {
         case '.':
             btn = decimal;
             break;
+        case '^':
+            btn = power;
+            break;
+        case '%':
+            btn = percent;
+            break;
         default:
             return;
     }
+
     btn.click();
     btn.classList.add('pressed');
 
